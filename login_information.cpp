60223a1be63e1d2feacbb3ec350f95eb97954dda
Hyunjik Lee <jiggy0429@gmail.com>
2018-06-23 05:26:24 +0900

    add user func

--- login_information.cpp
+++ login_information.cpp
@@ -24,11 +24,11 @@ string inputpasswords(void) {
 
 int login() {
 	string adminid, adminpass; //관리자 아이디와 비밀번호 설정되어 있음.
-	string id,pass; //사용자가 입력할 아이디와 비밀번호.
+	string id, pass; //사용자가 입력할 아이디와 비밀번호.
 	string userid, userpass; //사용자 아이디와 비밀번호
 
 	int passcnt = 0; //틀린횟수 카운트
-	
+
 
 	ifstream adminfin("supervisor.txt", ios::in); //만들어져있는 관리자 파일을 가져옴.
 	if (!adminfin)
@@ -39,7 +39,7 @@ int login() {
 	adminfin >> adminid;
 	adminfin >> adminpass;
 
-	
+
 	ifstream userfin("userid.txt"); //사용자 파일을 가져옴 관리자메뉴안에 사용자 만들기 기능이 있음 거기에 ofstream 쓰면 될 듯.
 	if (!userfin)
 	{
@@ -49,16 +49,16 @@ int login() {
 	int fileAccountNum = lineoftxt("userid.txt") / 2;
 	string * userids = new string[fileAccountNum];
 	string * userpasss = new string[fileAccountNum];
-	
+
 	for (int i = 0; i < fileAccountNum; i++) {
 		getline(userfin, userids[i]);
 		getline(userfin, userpasss[i]);
 	}
-	
+
 	while (1)
 	{
 		system("cls");
-		re:
+	re:
 		cout << "ID(계좌)를 입력하세요 : "; cin >> id;
 		cout << "비밀번호를 입력하세요 : "; pass = inputpasswords();
 
@@ -82,8 +82,8 @@ int login() {
 				}
 			}
 		}
-		
-		
+
+
 
 		//사용자 로그인
 		else if (!(adminid == id))
@@ -97,19 +97,19 @@ int login() {
 				}
 
 			}
-				if(!(adminpass==pass))
+			if (!(adminpass == pass))
+			{
+				++passcnt;
+				cout << "\n아이디 혹은 비밀번호가 " << passcnt << "회 틀렸습니다." << endl;
+
+				if (passcnt == 5)
 				{
-					++passcnt;
-					cout << "\n아이디 혹은 비밀번호가 " << passcnt << "회 틀렸습니다." << endl;
-
-					if (passcnt == 5)
-					{
-						cout << "프로그램을 종료합니다." << endl;
-						return 0;
-					}
-					goto re;
-
+					cout << "프로그램을 종료합니다." << endl;
+					return 0;
 				}
+				goto re;
+
+			}
 		}
 
 		else // 다 틀릴 때
--- user.cpp
+++ user.cpp
@@ -1,12 +1,21 @@
 #include "Accountlist.h"
 
-extern int lineoftxt(char* txtfile);
 
 int user(int cnt) {
 	Acc_list list;
+	pNode user = list.get_head();
+	for (int i = 0; i < cnt; i++) {
+		if(user)
+			user = user->link;
+	}
+	string pass;
+	string arthur;
+
 	cout << "사용자계정으로 접속중"; Sleep(500); cout << "."; Sleep(500); cout << "."; Sleep(500); cout << ".";
 	system("cls");
 	int sel = 0;
+	int don = 0;
+
 	while (sel != 5) {
 		cout << "--------------------------------------------------------------" << endl;
 		cout << "1.입금\t2.출금\t3.송금\t4.조회\t5.로그아웃" << endl;
@@ -17,12 +26,24 @@ int user(int cnt) {
 
 		switch (sel) {
 		case 1:
+			cout << "입금하실 금액을 입력해주세요 : "; cin >> don;
+			user->data.Deposit(don);
 			break;
 		case 2:
+			cout << "비밀번호를 입력하여 주십시오."; cin.ignore(); getline(cin, pass);
+			cout << "출금하실 금액을 입력해주세요 : "; cin >> don;
+			if (!(user->data.Withdraw(pass, don))) cout << "출금 실패...;;ㄲㅂ" << endl;
+			else cout << "출금 완료!" << endl;
 			break;
 		case 3:
+			cout << "송금할 계좌를 입력하여 주십시오."; cin.ignore(); getline(cin, arthur);
+			cout << "비밀번호를 입력하여 주십시오."; getline(cin, pass);
+			cout << "송금하실 금액을 입력해주세요 : "; cin >> don;
+			//user->data.Remmitance(arthur, pass, don);
 			break;
 		case 4:
+			cout << "비밀번호를 입력하여 주십시오."; cin.ignore(); getline(cin, pass);
+			user->data.Show(pass);
 			break;
 		case 5:
 			break;
