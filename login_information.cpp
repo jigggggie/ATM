d27d80b84e7f7c8e3108c78888be53d02d3db41a
Somin Kim <smkgs96@gmail.com>
2018-06-23 03:13:58 +0900

    final login

--- login_information.cpp
+++ login_information.cpp
@@ -4,47 +4,16 @@
 using namespace std;
 extern string inputpasswords(void);
 
-//void createAccount() // Takes and stores users login information, username and password
-//{
-//	string username;
-//	string password;
-//	cout << "Please create a username" << endl;
-//	cin >> username;
-//	cout << "Please create a password" << endl;
-//	cin >> password;
-//	ofstream createaccount;
-//	createaccount.open("accounts.txt", ios::app); // Will store the users information.
-//	createaccount << username << " " << password << endl;
-//	createaccount.close();
-//	cout << "Account created!" << endl;
-//
-//}
-
-
-//extern string Id, password;
-
-//int main() {
-//	string Id;
-//	string password;
-//
-//	ofstream fout("supervisor.txt");
-//	if (!fout)
-//	{
-//		cout << "파일 열기 실패" << endl;
-//		return 0;
-//	}
-//
-//	cout << "ID입력 : "; cin >> Id;
-//	cout << "비밀번호 : "; password = inputpasswords();
-//	fout << Id << endl;
-//	fout << password << endl;
-//	
-//	fout.close();
-//	return 0;
-//}
-
-
-
+int lineoftxt() {
+	ifstream wow;
+	wow.open("userid.txt", ios::in);
+	string temp;
+	int linenum = 0;
+	while (getline(wow, temp))
+		++linenum;
+	wow.close();
+	return linenum;
+}
 
 int main() {
 	string adminid, adminpass; //관리자 아이디와 비밀번호 설정되어 있음.
@@ -52,7 +21,7 @@ int main() {
 	string userid, userpass; //사용자 아이디와 비밀번호
 
 	int passcnt = 0; //틀린횟수 카운트
-	int login_menu;
+	
 
 	ifstream adminfin("supervisor.txt", ios::in); //만들어져있는 관리자 파일을 가져옴.
 	if (!adminfin)
@@ -64,95 +33,128 @@ int main() {
 	adminfin >> adminpass;
 
 	
-ifstream userfin("user.txt"); //사용자 파일을 가져옴 관리자메뉴안에 사용자 만들기 기능이 있음 거기에 ofstream 쓰면 될 듯.
+	ifstream userfin("userid.txt"); //사용자 파일을 가져옴 관리자메뉴안에 사용자 만들기 기능이 있음 거기에 ofstream 쓰면 될 듯.
 	if (!userfin)
 	{
 		cout << "사용자파일 열기 실패" << endl;
 		return 0;
 	}
-	userfin >> userid;
-	userfin >> userpass;
+	int fileAccountNum = lineoftxt() / 2;
+	string * userids = new string[fileAccountNum];
+	string * userpasss = new string[fileAccountNum];
+	/*userfin >> userid;
+	userfin >> userpass;*/
 	
-	cout << "1.관리자 로그인\t 2.사용자 로그인" << endl; cin >> login_menu;
-	switch (login_menu)
-	{
-	case 1:
-		while (6)
-		{
-			cout << "ID입력 : "; cin >> id;
-			cout << "비밀번호 : "; pass = inputpasswords();
-
-			if (adminid == id) //관리자 로그인
-			{
-				if (adminpass == pass)
-				{
-					cout << "\n관리자 로그인 성공!" << endl;
-					break;
-				}
-				else
-				{
-					++passcnt;
-					cout << "\n아이디 혹은 비밀번호가 " << passcnt << "회 틀렸습니다." << endl;
-					if (passcnt == 5)
-					{
-						cout << "프로그램을 종료합니다." << endl;
-						return 0;
-					}
-				}
-			}
-			else
-			{
-				++passcnt;
-				cout << "\n아이디 혹은 비밀번호가 " << passcnt << "회 틀렸습니다." << endl;
-				if (passcnt == 5)
-				{
-					cout << "프로그램을 종료합니다." << endl;
-					return 0;
-				}
-			}
-		
-		}
-		break;
-
-
-	case 2:
-		while (6)
-		{
-			cout << "유저아이디: " << userid << "유저비번:" << userpass;
-			cout << "사용계좌번호를 입력하시오: "; cin >> id;
-			cout << "사용비밀번호를 입력하시오: "; pass = inputpasswords();
-
-			if (userid == id) //사용자 로그인
-			{
-				if (userpass == pass)
-				{
-					cout << "\n사용자 로그인 성공!" << endl;
-					break;
-				}
-
-				else
-				{
-					++passcnt;
-					cout << "\n아이디 혹은 비밀번호가 " << passcnt << "회 틀렸습니다." << endl;
-					if (passcnt == 5)
-					{
-						cout << "프로그램을 종료합니다." << endl;
-						return 0;
-					}
-				}
-			}
-			else
-			{
-				++passcnt;
-				cout << "\n아이디 혹은 비밀번호가 " << passcnt << "회 틀렸습니다." << endl;
-				if (passcnt == 5)
-				{
-					cout << "프로그램을 종료합니다." << endl;
-					return 0;
-				}
-			}
-		}
-		break;
+	for (int i = 0; i < fileAccountNum; i++) {
+		getline(userfin, userids[i]);
+		getline(userfin, userpasss[i]);
 	}
+	
+	while (1)
+	{
+		re:
+		cout << "ID(계좌)를 입력하세요 : "; cin >> id;
+		cout << "비밀번호를 입력하세요 : "; pass = inputpasswords();
+
+
+		//관리자 로그인
+		if (adminid == id)
+		{
+			if (adminpass == pass)
+			{
+				cout << "\n관리자 로그인 성공!" << endl;
+				break; //추후에 관리자 함수로 가는 부분
+			}
+			else //id는 맞고 비번이 틀릴 때
+			{
+				++passcnt;
+				cout << "\n아이디 혹은 비밀번호가 " << passcnt << "회 틀렸습니다." << endl;
+				if (passcnt == 5)
+				{
+					cout << "프로그램을 종료합니다." << endl;
+					return 0;
+				}
+				//goto try_again;
+			}
+		}
+		//else if (adminid != id && adminpass == pass) //id는 틀리고 비번이 맞을 때
+		//{
+		//	++passcnt;
+		//	cout << "\n아이디 혹은 비밀번호가 " << passcnt << "회 틀렸습니다." << endl;
+		//	if (passcnt == 5)
+		//	{
+		//		cout << "프로그램을 종료합니다." << endl;
+		//		return 0;
+		//	}
+		//}
+		
+
+		//사용자 로그인
+		else if (!(adminid == id))
+		{
+			for (int cnt = 0; cnt < fileAccountNum; cnt++)
+			{
+				if (userids[cnt] == id && userpasss[cnt] == pass)
+				{
+					cout << "\n 사용자 로그인 성공!" << endl;
+					cout << "cnt출력: " << cnt;
+					break; //사용자 함수로 가는 부분
+				}
+
+			}
+		if(!(adminpass==pass))
+		{
+			++passcnt;
+			cout << "\n아이디 혹은 비밀번호가 " << passcnt << "회 틀렸습니다." << endl;
+
+			if (passcnt == 5)
+			{
+				cout << "프로그램을 종료합니다." << endl;
+				return 0;
+			}
+			goto re;
+
+				//if (userpass == pass)
+				//{
+				//	cout << "\n사용자 로그인 성공!" << endl;
+				//	break;
+				//}
+
+				//else //id는 맞고 비번이 틀릴 때
+				//{
+				//	++passcnt;
+				//	cout << "\n아이디 혹은 비밀번호가 " << passcnt << "회 틀렸습니다." << endl;
+				//	if (passcnt == 5)
+				//	{
+				//		cout << "프로그램을 종료합니다." << endl;
+				//		return 0;
+				//	}
+				//}
+			}
+			
+		}
+		//else if (userid != id && userpass == pass) //id가 틀리고 비번이 맞을 때
+		//{
+		//	++passcnt;
+		//	cout << "\n아이디 혹은 비밀번호가 " << passcnt << "회 틀렸습니다." << endl;
+		//	if (passcnt == 5)
+		//	{
+		//		cout << "프로그램을 종료합니다." << endl;
+		//		return 0;
+		//	}
+		//}
+		else // 다 틀릴 때
+		{
+			++passcnt;
+			cout << "\n아이디 혹은 비밀번호가 " << passcnt << "회 틀렸습니다." << endl;
+			if (passcnt == 5)
+			{
+				cout << "프로그램을 종료합니다." << endl;
+				return 0;
+			}
+
+		}
+	}
+		
 
 }
